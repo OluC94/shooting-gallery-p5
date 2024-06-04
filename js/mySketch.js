8a6e5faa8788bea44160eb132cbb7340d65506ed
OluC94 <coker.olu94@gmail.com>
2024-06-04 15:44:45 +0100

    add target shooter code from p5

--- js/mySketch.js
+++ js/mySketch.js
@@ -1,30 +1,72 @@
 //This is our main javascript file constituting our p5.js sketch.
 //It must be loaded from index.html
 //It assumes that the file "myPalettes.js" has also been loaded
-
+const targets = []
 let currentPalette;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight / 2);
+    createCanvas(windowWidth, windowHeight);
     currentPalette = randomPalette();
-    noStroke();
-    background("white");
+    // noStroke();
+    createTargets();
+    
 }
 
 function draw() {
-    fill(random(currentPalette));
-    const x = random(0, width);
-    const y = random(0, height);
-    circle(x, y, 100);
+    background("white");
+    noStroke()
+    drawTargets();
+
 }
 
+const createTargets = () => {
+	while (targets.length < 10) {
+
+		const target = {
+			x: random(0, width),
+			y: random(0, height),
+			diameter: random(10, 50),
+			colour: random(currentPalette),
+			isAlive: true,
+		}
+		targets.push(target)
+	}
+}
+
+const drawTargets = () => {
+	for (let target of targets) {
+		if (target.isAlive) {
+			fill(target.colour)
+			circle(target.x, target.y, target.diameter)
+		}
+	}
+}
+
+
 function mouseClicked() {
-    background(255);
-    currentPalette = randomPalette();
+    
+
 }
 
 function keyPressed() {
     if (key === "s") {
         save("my-p5-screenshot");
+    }
+}
+
+class Target {
+    constructor(x, y, diameter){
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        this.colour = random(currentPalette),
+        this.isAlive = true;
+    }
+
+    display(){
+        if (this.isAlive){
+            fill(this.colour);
+            circle(this.x, this.y, this.diameter)
+        }
     }
 }
