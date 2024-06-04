d35d2cc9d19617d443d26594366c8ef39217d5da
OluC94 <coker.olu94@gmail.com>
2024-06-04 16:08:09 +0100

    format

--- js/mySketch.js
+++ js/mySketch.js
@@ -1,7 +1,7 @@
 //This is our main javascript file constituting our p5.js sketch.
 //It must be loaded from index.html
 //It assumes that the file "myPalettes.js" has also been loaded
-const targets = []
+const targets = [];
 let currentPalette;
 
 function setup() {
@@ -9,35 +9,32 @@ function setup() {
     currentPalette = randomPalette();
     // noStroke();
     createTargets();
-    
 }
 
 function draw() {
     background("white");
-    noStroke()
+    noStroke();
     drawTargets();
-
 }
 
 const createTargets = () => {
-	while (targets.length < 10) {
-        const x = random(20, width - 20)
-        const y  = random(20, height - 20)
-        const diameter = random(10, 50)
+    while (targets.length < 10) {
+        const x = random(20, width - 20);
+        const y = random(20, height - 20);
+        const diameter = random(10, 50);
 
-        targets.push(new Target(x, y, diameter))
-		
-	}
-}
+        targets.push(new Target(x, y, diameter));
+    }
+};
 
 const drawTargets = () => {
-	for (let target of targets) {
-		target.display()
-	}
-}
+    for (let target of targets) {
+        target.display();
+    }
+};
 
 function mouseClicked() {
-    for (let target of targets){
+    for (let target of targets) {
         target.clicked(mouseX, mouseY);
     }
 }
--- js/target.js
+++ js/target.js
@@ -1,25 +1,24 @@
 class Target {
-    constructor(x, y, diameter){
+    constructor(x, y, diameter) {
         this.x = x;
         this.y = y;
         this.diameter = diameter;
-        this.colour = random(currentPalette),
-        this.isAlive = true;
+        (this.colour = random(currentPalette)), (this.isAlive = true);
     }
 
-    display(){
-        if (this.isAlive){
+    display() {
+        if (this.isAlive) {
             fill(this.colour);
-            circle(this.x, this.y, this.diameter)
+            circle(this.x, this.y, this.diameter);
         }
     }
 
-    clicked(posX, posY){
-        let rad = this.diameter / 2
+    clicked(posX, posY) {
+        let rad = this.diameter / 2;
         let mouseDistance = dist(posX, posY, this.x, this.y);
         let targetIsClicked = mouseDistance < rad;
-        
-        if (targetIsClicked){
+
+        if (targetIsClicked) {
             this.isAlive = false;
         }
     }
